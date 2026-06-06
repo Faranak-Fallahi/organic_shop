2a6a7af271221159eb2ffc2fa8f21480f107e72f
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-06-06 17:10:57 +0330

    feat(store):add productlist view

--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . import models
 
-# Create your views here.
+
+def product_list(request):
+    products = models.Product.objects.filter(is_active=True)
+    categories = models.Category.objects.all()
+    
+
+    context = {
+        "products": products,
+        "categories": categories,
+        
+    }
+
+    return render(request, "store/product_list.html", context)
+
