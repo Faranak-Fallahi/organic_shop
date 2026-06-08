1fb4340473875374235149a0eabd3318dca1bfaf
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-06-08 12:05:00 +0330

    feat(accounts): implement user logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from .views import UserLoginView
+from .views import UserLoginView 
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
