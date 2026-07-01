09f0dfea2952f3e6e4a43ed0c31991c8a8a18dfb
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-01 18:16:37 +0330

    feat(comments): add post and product comments url

--- comments/urls.py
+++ comments/urls.py
@@ -4,21 +4,11 @@ from . import views
 app_name = "comments"
 
 urlpatterns = [
-    path(
-        "add/<slug:slug>/",
-        views.add_post_comment,
-        name="add_post_comment"
-    ),
+     path("add/<slug:slug>/", views.add_post_comment, name="add_post_comment"),
+    path("delete/<int:comment_id>/", views.delete_post_comment, name="delete_post_comment"),
+    path("edit/<int:comment_id>/", views.edit_post_comment, name="edit_post_comment"),
+    path("product/add/<slug:slug>/", views.add_product_comment, name="add_product_comment"),
+    path("product/delete/<int:comment_id>/", views.delete_product_comment, name="delete_product_comment"),
+    path("product/edit/<int:comment_id>/", views.edit_product_comment, name="edit_product_comment"),
 
-    path(
-        "delete/<int:comment_id>/",
-        views.delete_post_comment,
-        name="delete_post_comment"
-    ),
-
-    path(
-        "edit/<int:comment_id>/",
-        views.edit_post_comment,
-        name="edit_post_comment"
-    ),
 ]
