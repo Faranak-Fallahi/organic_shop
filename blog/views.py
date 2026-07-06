f10d4cf368a3ce830d399388c89ec02e4131ff4c
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-06 13:51:50 +0330

    feat(api/blog): add post viewset with dynamic serializer choice

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,13 @@
 from .models import Post
-from comments.models import PostComment
-from django.shortcuts import render,get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostListSerializer, PostDetailSerializer
+from django.db.models import Count
 
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all().annotate(comments_count=Count('Post_Comment'))
+    lookup_field = 'slug'
 
-def post_list(request):
-    posts = Post.objects.filter(published=True)
-
-    return render(request, 'blog/post_list.html', {
-        'posts': posts
-    })
-
-def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    comments = PostComment.objects.filter(post = post, is_active=True, parent=None,)
-
-    return render(request, 'blog/post_detail.html', {
-        'post': post,
-        'comments': comments,
-    })
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        return PostDetailSerializer
