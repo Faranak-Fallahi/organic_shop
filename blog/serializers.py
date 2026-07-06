ff86c5e7ab3ffa33dc3a7c8fd10ad782dbb0b8a0
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-06 13:40:10 +0330

    feat(api/blog): create list and detail serializers for post

---
+++ blog/serializers.py
@@ -0,0 +1,49 @@
+from .models import Post
+from rest_framework import serializers
+
+class PostListSerializer(serializers.ModelSerializer):
+    comments_count = serializers.IntegerField(read_only=True)
+    summary = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 
+            'title', 
+            'slug',
+            'summary', 
+            'created_at', 
+            'updated_at', 
+            'published', 
+            'image',
+            'comments_count',
+        ]
+        read_only_fields = ['id', 'slug', 'created_at', 'updated_at']
+        
+    def get_summary(self, obj):
+        if obj.content:
+            return obj.content[:150] + '...' if len(obj.content) > 150 else obj.content
+        return 
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    comments_count = serializers.IntegerField(read_only=True)
+    
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 
+            'title', 
+            'slug', 
+            'content', 
+            'comments_count',
+            'reading_time',
+            'created_at', 
+            'updated_at', 
+            'published', 
+            'image'
+        ]
+        read_only_fields = ['id', 'slug', 'created_at', 'updated_at']
+
+    
