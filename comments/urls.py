4e64c10e6abe22f882c9781546fea3b9f2d6c11e
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-08 09:16:36 +0330

    feat(comments):add comment url with defaultrouter

--- comments/urls.py
+++ comments/urls.py
@@ -1,14 +1,14 @@
-from . import views
-from django.urls import path
+from .views import PostCommentViewSet, ProductCommentViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-app_name = "comments"
+
+router = DefaultRouter()
+
+router.register('post-comments', PostCommentViewSet, basename='post-comments')
+router.register('product-comments', ProductCommentViewSet, basename='product-comments')
+
 
 urlpatterns = [
-     path("add/<slug:slug>/", views.add_post_comment, name="add_post_comment"),
-    path("delete/<int:comment_id>/", views.delete_post_comment, name="delete_post_comment"),
-    path("edit/<int:comment_id>/", views.edit_post_comment, name="edit_post_comment"),
-    path("product/add/<slug:slug>/", views.add_product_comment, name="add_product_comment"),
-    path("product/delete/<int:comment_id>/", views.delete_product_comment, name="delete_product_comment"),
-    path("product/edit/<int:comment_id>/", views.edit_product_comment, name="edit_product_comment"),
-
+    path('', include(router.urls)),
 ]
