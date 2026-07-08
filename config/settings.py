d4520becca440a2b3d57bc9e0d5e7510a2cb4007
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-08 18:43:28 +0330

    chore: configure rest framework pagination settings

--- config/settings.py
+++ config/settings.py
@@ -163,4 +163,5 @@ LOGOUT_REDIRECT_URL = "product_list"
 #rest framework
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING' : False ,
+    'DEFAULT_PAGINATION_CLASS' :'rest_framework.pagination.PageNumberPagination',
 }
