75e9dc331c71c427f4f9c0abf0de7738db19cc3c
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-12 20:34:37 +0330

    refactor: update models to use UUID

--- cart/models.py
+++ cart/models.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.db import models
 from store.models import Product
+import uuid
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
