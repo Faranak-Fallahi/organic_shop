8c6b6f6a8aecb6c8d5c4aeaeeae6bc5dc8198cd1
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-14 21:40:20 +0330

    fix(cart) :fix cart model

--- cart/models.py
+++ cart/models.py
@@ -10,6 +10,8 @@ class Cart(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='cart'
+        null=True,
+        blank=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
