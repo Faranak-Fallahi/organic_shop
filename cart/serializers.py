2f24d4ea6ca5dc21f87a3e6308d6b6b065dc401c
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-14 22:11:25 +0330

    feat(cart) : add model viewset for cart

--- cart/serializers.py
+++ cart/serializers.py
@@ -14,22 +14,27 @@ class CartItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'product_title', 'product_price', 'quantity']
-        read_only_fields = ['id', 'product', 'product_title', 'product_price', 'quantity','total_item']
+        fields = ['id', 'product', 'product_title', 'product_price', 'quantity','total_item']
+        read_only_fields = ['id', 'product' , 'quantity']
 
     def get_total_item(self,cart_item):
-        return cart_item.quantity*cart_item.product_price
+        return cart_item.quantity*cart_item.product.price
     
     
     
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True, read_only=True)
-    user = serializers.StringRelatedField()
+    user = serializers.StringRelatedField(read_only=True)
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'items']
-        read_only_fields = ['id', 'user', 'items']
+        fields = ['id', 'user', 'items','total_price']
+        read_only_fields = ['id']
+        
+        
+    def get_total_price(self,cart):
+        return sum(item.quantity*item.product.price for item in cart.items.all())
 
 
 class AddCartItemSerializer(serializers.ModelSerializer):
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Cart, CartItem
+from .serializers import (
+    CartSerializer,
+    CartItemSerializer,
+    AddCartItemSerializer,
+    UpdateCartItemSerializer,
+)
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
+    permission_classes = [AllowAny]
+    http_method_names = ['post', 'get']
+
+    def create(self, request, *args, **kwargs):
+        cart = Cart.objects.create()
+        serializer = self.get_serializer(cart)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        cart_pk = self.kwargs.get('cart_pk')
+        return CartItem.objects.filter(cart_id=cart_pk).select_related('product')
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return AddCartItemSerializer
+        if self.action in ['update', 'partial_update']:
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        cart_pk = self.kwargs.get('cart_pk')
+        cart = get_object_or_404(Cart, pk=cart_pk)
+
+        product = serializer.validated_data['product']
+        quantity = serializer.validated_data['quantity']
+
+        cart_item, created = CartItem.objects.get_or_create(
+            cart=cart,
+            product=product,
+            defaults={'quantity': quantity}
+        )
+
+        if not created:
+            cart_item.quantity += quantity
+            cart_item.save()
+
+        output_serializer = CartItemSerializer(cart_item)
+        return Response(output_serializer.data, status=status.HTTP_201_CREATED)
