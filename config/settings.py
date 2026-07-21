1fdce624ecae5b276860d0095938cb1813a65c18
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-21 11:58:27 +0330

    feat(accounts): add djoser user create serializer

--- config/settings.py
+++ config/settings.py
@@ -174,3 +174,10 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES':('JWT',),
 }
+
+DJOSER = {
+    'SERIALIZERS' :{
+        'user_create' :'accounts.serializers.UserCreateSerializer',
+        'current_user' :'accounts.serializers.UserSerializer',
+    }
+}
