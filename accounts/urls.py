ac241936743b4ce7008220395721fbd54632c24c
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-07-27 12:56:35 +0330

    feat(accounts):add customer urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,23 +1,23 @@
 from . import views
-from django.contrib.auth.views import LogoutView 
+from .views import CustomerViewSet
+from django.contrib.auth.views import LogoutView
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 app_name = "accounts"
 
-
+router = DefaultRouter()
+router.register('customers', CustomerViewSet, basename='customers')
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path("login/", views.UserLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-    path("profile/", views.profile_view, name="profile"),
-    path('password-change/',views.CustomPasswordChangeView.as_view(), name='password_change'),
-     path('password-reset/', views.PhonePasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+    path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password-reset/', views.PhonePasswordResetRequestView.as_view(), name='password_reset_request'),
     path('password-reset/verify/', views.OTPVerifyView.as_view(), name='password_reset_verify'),
     path('password-reset/confirm/', views.SetNewPasswordView.as_view(), name='password_reset_confirm'),
-    
-    
-   
-
-
 ]
+
+urlpatterns += router.urls
