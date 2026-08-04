38ff22a7a0c29622e4bf85c942d4b58aa7593700
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-08-04 14:24:02 +0330

    feat(orders):migrate orderItem model

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator
+from store.models import Product 
 
 class Order(models.Model):
    
@@ -49,3 +50,32 @@ class Order(models.Model):
     @property
     def total_price(self):
         return sum(item.total_price for item in self.items.all())
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT,
+        related_name='order_items'
+    )
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    # ذخیره قیمت در لحظه خرید
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.product.name} (x{self.quantity})"
+
+   
+    @property
+    def total_price(self):
+        return self.price * self.quantity
+    
