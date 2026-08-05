6826d1edd0265b9e1635832371175a53eade1bca
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-08-05 15:18:27 +0330

    feat(orders): add OrderViewSet

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,22 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
+from rest_framework import  viewsets
+from .models import Order, OrderItem
+from .serializers import OrderSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset =Order.objects.select_related('user').prefetch_related(
+            Prefetch(
+                'items',
+                
+                queryset=OrderItem.objects.select_related('product')),
+            )
+        user = self.request.user
+        if user.is_staff:
+            return queryset
+        return queryset.filter(user=user)
