bb5bf8d3871883599c5a8da43d2ad5e556d39d73
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-08-06 20:44:32 +0330

    feat(orders): add order create serializer to orderviewset

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Prefetch
 from rest_framework import  viewsets
 from .models import Order, OrderItem
-from .serializers import OrderForAdminSerializer, OrderSerializer
+from .serializers import OrderCreateSerializer, OrderForAdminSerializer, OrderSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class OrderViewSet(viewsets.ModelViewSet):
@@ -20,6 +20,8 @@ class OrderViewSet(viewsets.ModelViewSet):
         return queryset.filter(user=user)
 
     def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return OrderCreateSerializer
         if self.request.user.is_staff:
             return OrderForAdminSerializer
         return OrderSerializer
