a4f575b2e3539f1fc09aff8f542a50b03475c2a4
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-08-24 12:01:55 +0330

    feat(contant): add and migrate  cntant models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Branch(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان شعبه")
+    phone = models.CharField(
+        max_length=50, blank=True, verbose_name="تلفن تماس"
+    )
+    whatsapp = models.CharField(
+        max_length=50, blank=True, verbose_name="شماره واتساپ"
+    )
+    working_hours = models.CharField(
+        max_length=150, blank=True, verbose_name="روزها و ساعات کاری"
+    )
+    address = models.TextField(verbose_name="آدرس کامل")
+    neshan_link = models.URLField(
+        blank=True, verbose_name="لینک مسیریابی در نشان"
+    )
+    order = models.PositiveIntegerField(default=0, verbose_name="ترتیب نمایش")
+    is_active = models.BooleanField(default=True, verbose_name="فعال باشد؟")
+
+    class Meta:
+        verbose_name = "شعبه"
+        verbose_name_plural = "شعبه‌ها"
+        ordering = ["order", "id"]
+
+    def __str__(self):
+        return self.title
