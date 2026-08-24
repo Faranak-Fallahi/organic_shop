3483f7c361ab86e05ce59990752982ba4c39c257
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-08-24 12:04:22 +0330

    feat(contant): add contant admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Branch
 
-# Register your models here.
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ("title", "phone", "whatsapp", "is_active", "order")
+    list_filter = ("is_active",)
+    search_fields = ("title", "address", "phone")
+    list_editable = ("is_active", "order")
