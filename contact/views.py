6d09ad4ab19719a452fc538b3c2e741ab7840558
Faranak Fallahi <fallahifaranak422@gmail.com>
2026-08-24 12:15:11 +0330

    feat(contant): add about us view

--- contact/views.py
+++ contact/views.py
@@ -5,3 +5,6 @@ from .models import Branch
 def contact_us_view(request):
     branches = Branch.objects.filter(is_active=True)
     return render(request, "contact/contact_us.html", {"branches": branches})
+
+def about_us_view(request):
+    return render(request, "contact/about_us.html")
